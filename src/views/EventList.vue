267d294d68c3b5494cfcf3c5ee06314251c6d26b
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2022-02-04 21:51:16 -0600

    chore: making api call

---
+++ src/views/EventList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <section class="event-container">
+      <EventCard v-for="event in events" :event="event" :key="event.title" />
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue';
+import EventService from '@/services/EventService.js';
+
+export default {
+  name: 'Home',
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+<style scoped>
+.event-container {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
