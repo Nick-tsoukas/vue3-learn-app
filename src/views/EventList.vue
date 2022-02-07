2088f47d744ff5ded731ae21a1d7e87d6827b50d
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2022-02-07 04:55:25 -0600

    chore: adding nested routes

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,6 +3,22 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <section class="event-container">
       <EventCard v-for="event in events" :event="event" :key="event.title" />
+      <div class="pagination_container">
+        <router-link
+          :to="{ name: 'EventList', query: { page: page - 1 } }"
+          rel="prev"
+          v-if="page != 1"
+        >
+          Prev Page
+        </router-link>
+        <router-link
+          :to="{ name: 'EventList', query: { page: page + 1 } }"
+          rel="next"
+          v-if="hasNextPage"
+        >
+          Next Page
+        </router-link>
+      </div>
     </section>
   </div>
 </template>
@@ -11,29 +27,46 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue';
 import EventService from '@/services/EventService.js';
-
+import { watchEffect } from 'vue';
 export default {
   name: 'Home',
+  props: ['page'],
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
+      totalEvents: 0,
     };
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    watchEffect(() => {
+      this.events = null;
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data;
+          this.totalEvents = response.headers['x-total-count'];
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2);
+      return this.page < totalPages;
+    },
   },
 };
 </script>
 <style scoped>
+.pagination_container {
+  display: flex;
+  justify-content: space-between;
+  width: 290px;
+}
 .event-container {
   width: 100vw;
   display: flex;
